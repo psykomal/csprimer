6ca30229fa07b061c699989c4fb942fc8e654be6
psykomal <bkomalsai@gmail.com>
2023-06-06 00:53:10 +0530

    basic shell

--- os/shell/basic-shell.c
+++ os/shell/basic-shell.c
@@ -1,17 +1,35 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <unistd.h>
+#include <signal.h>
 
 #define MAX_CMD 4096
 #define MAX_ARGV 256
-#define PROMPT "> "
+#define PROMPT "💀 "
 #define SEP " \t\n"
+
+volatile pid_t childpid = 0;
+
+void sigint_handler(int sig)
+{
+    if (!childpid)
+        return;
+
+    if (kill(childpid, SIGINT), 0)
+    {
+        perror("Error sending SIGINT to child");
+    }
+    return;
+}
 
 int main()
 {
     char cmd[MAX_CMD];
     int argc;
     char *argv[MAX_ARGV];
+
+    signal(SIGINT, sigint_handler);
 
     while (1)
     {
@@ -41,6 +59,8 @@ int main()
         // printf("\n");
 
         // eval
+        if (strlen(argv) == 0)
+            continue;
         if (0 == strcmp(argv[0], "quit"))
             exit(0);
         if (0 == strcmp(argv[0], "help"))
@@ -48,5 +68,25 @@ int main()
             printf("Builins are 'quit' and 'help'\n");
             continue;
         }
+
+        if ((childpid = fork()) < 0)
+        {
+            perror("fork error");
+            exit(1);
+        }
+
+        if (childpid == 0)
+        {
+            if (execvp(argv[0], argv) < 0)
+            {
+                perror("execvp error");
+                exit(1);
+            }
+        }
+
+        // parent
+        int status;
+        waitpid(childpid, &status, 0);
+        childpid = 0;
     }
 }
