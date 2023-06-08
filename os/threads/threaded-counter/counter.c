375684ff190e9856d202d21a1e51eb66c9136735
psykomal <bkomalsai@gmail.com>
2023-06-08 22:57:26 +0530

    threaded counter

--- os/threads/threaded-counter/counter.c
+++ os/threads/threaded-counter/counter.c
@@ -4,15 +4,19 @@
 #define EACH_COUNT 1000000000
 
 volatile int counter = 0;
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
 
-void* thread_entry(void *arg) {
+void *thread_entry(void *arg)
+{
+  pthread_mutex_lock(&lock);
   for (int i = 0; i < EACH_COUNT; i++)
     counter++;
+  pthread_mutex_unlock(&lock);
   return NULL;
 }
 
-
-int main () {
+int main()
+{
   pthread_t p1, p2;
   pthread_create(&p1, NULL, thread_entry, NULL);
   pthread_create(&p2, NULL, thread_entry, NULL);
@@ -20,4 +24,3 @@ int main () {
   pthread_join(p2, NULL);
   printf("Final count: %d (expected %d)\n", counter, 2 * EACH_COUNT);
 }
-
