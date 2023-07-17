c7c4cdc3355e1becf2af746efa89fb62ce0af631
psykomal <bkomalsai@gmail.com>
2023-07-17 15:31:10 +0530

    faster sum

--- low-level/uarch/faster-sum/sum.c
+++ low-level/uarch/faster-sum/sum.c
@@ -1,6 +1,19 @@
-int sum(int *nums, int n) {
-  int total = 0;
-  for (int i = 0; i < n; i++)
-    total += nums[i];
-  return total;
+// int sum(int *nums, int n) {
+//   int total = 0;
+//   for (int i = 0; i < n; i++)
+//     total += nums[i];
+//   return total;
+// }
+
+int sum(int *nums, int n)
+{
+  int t1, t2, t3, t4 = 0;
+  for (int i = 0; i < n; i += 4)
+  {
+    t1 += nums[i];
+    t2 += nums[i + 1];
+    t3 += nums[i + 2];
+    t4 += nums[i + 3];
+  }
+  return t1 + t2 + t3 + t4;
 }
